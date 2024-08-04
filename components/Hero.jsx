971dbc5c696d294a85ee8c277674eed1bf3a7bae
Paul Fruitful <fruitful2007@outlook.com>
2024-08-04 12:16:08 +0100

    From the useState

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,8 @@
-const Hero = ({open}) => {
+const Hero = ({open,count}) => {
   return (
     <div className="flex justify-between px-3 mt-3 lg:px-8 w-full ">
 
-        <span className="text-lg border-b-2 lg:p-0 pl-2  border-solid  ">Pantry🥗<span className=" bg-white ml-1  text-black rounded-full py-1 px-2 text-md">1</span></span>
+        <span className="text-lg border-b-2 lg:p-0 pl-2  border-solid  ">Pantry🥗<span className=" bg-white ml-1  text-black rounded-full py-1 px-2 text-md">{count}</span></span>
 
         <button onClick={()=>{open()}} className=" p-3 bg-green-600  text-white rounded-[10px] shadow-sm">+ Add Pantry</button>
      
