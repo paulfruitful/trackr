f3d19a7a437cf9fa0e635ce1caac399eadd65bb8
Paul Fruitful <fruitful2007@outlook.com>
2024-08-05 16:49:50 +0100

    lint delployment error fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -72,6 +72,7 @@ export default function Example() {
 
   return (
     <>
+    {/* eslint-disable-next-line react/no-unescaped-entities */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <svg
